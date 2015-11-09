0d50810f569ac7d871aeacfc431d3e9460edec63
gcampes <gabrieldecampes@gmail.com>
2015-11-09 17:43:20 -0200

    Added routeAfter for redirecting

--- app/controllers/person/add.js
+++ app/controllers/person/add.js
@@ -3,6 +3,7 @@ import EmberValidations from 'ember-validations';
 import MagicCrud from 'ember-imdt-crud/mixins/ember-imdt-magic-crud';
 
 export default Ember.Controller.extend(EmberValidations, MagicCrud, {
+  routeAfter: 'person',
   definitions: [
     {
       value: 'model.firstName',
